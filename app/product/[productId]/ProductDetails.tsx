fb78aff7f3aba2e149358f11c118b8c79762773d
Hakeem <hazaak009@gmail.com>
2024-04-12 18:50:27 +0800

    done

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -1,14 +1,26 @@
 "use client";
 
+import { Rating } from "@mui/material";
+
 interface productDetailsProps {
   product: any;
 }
-
+//**issues here
 const ProductDetails: React.FC<productDetailsProps> = ({ product }) => {
+  const productRating =
+  product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
+  product.reviews.length;
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
       <div>Images</div>
-      <div>{product.name}</div>
+    
+      <div>
+        <h2 className="text-3xl font-medium text-slate-700">{product.name}</h2>
+        <div>
+          <Rating value={productRating} readOnly/>
+        </div>
+      </div>
+      
     </div>
   );
 };
