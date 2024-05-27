1916a474112dba6f01f02d03ac80b64eb97d9ae7
Hakeem <hazaak009@gmail.com>
2024-05-27 10:12:18 +0800

    d

--- app/Components/Button.tsx
+++ app/Components/Button.tsx
@@ -9,11 +9,8 @@ interface ButtonProps {
     small?: boolean;
     custom?: string;
     icon?: IconType;
-    onClick: (e: React.MouseEvent<HTMLButtonElement>) 
-    => void;
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
-
-
 
 const Button: React.FC<ButtonProps> = ({
     label,
@@ -23,36 +20,33 @@ const Button: React.FC<ButtonProps> = ({
     custom,
     icon: Icon,
     onClick,
-    
 }) => {
     return (
         <button 
-        disabled = {disabled}
-        className={`
-        disabled: opacity-70
-        disabled: cursor-not-allowed
-        rounded-md
-        hover:opacity-80
-        transition
-        w-full
-        border-slate-700
-        flex
-        items-center
-        justify-center
-        gap-2
-        ${outline?  "bg-white" : 'bg-slate-700'}
-        ${outline? "text-slate-700" : "text-white"}
-        ${small? 'text-sm font-light' : 
-        'text-md font-semibold'}
-        ${small? 'py-2 border-[1px]' : 'py-3 px-4 border-2'}
-        ${custom ? custom : ""}
-        `}
+            disabled={disabled}
+            onClick={onClick}
+            className={`
+                rounded-md
+                hover:opacity-80
+                transition
+                w-full
+                border-slate-700
+                flex
+                items-center
+                justify-center
+                gap-2
+                ${outline ? "bg-white" : 'bg-slate-700'}
+                ${outline ? "text-slate-700" : "text-white"}
+                ${small ? 'text-sm font-light' : 'text-md font-semibold'}
+                ${small ? 'py-2 border-[1px]' : 'py-3 px-4 border-2'}
+                ${custom ? custom : ""}
+                ${disabled ? 'opacity-70 cursor-not-allowed' : 'cursor-pointer'} /* Applied the fix here */
+            `}
         >
-            {Icon && <Icon size= {24}/>}
-            {label}       
+            {Icon && <Icon size={24} />}
+            {label}
         </button>
-    )
-    
+    );
 }
- 
+
 export default Button;
--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -12,7 +12,6 @@ interface productDetailsProps {
 }
 
 
-
 export type CartProductType = {
   id: string,
   name: string,
